1ad9b41c95b23821dc1db0664f6bd3fa97dbc951
awittig <awittig@gmail.com>
2014-12-21 13:42:47 -0600

    renaming stuff

--- src/main/java/net/wittig/monster/controller/EncounterController.java
+++ src/main/java/net/wittig/monster/controller/EncounterController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EncounterController {
 
-    @RequestMapping("/encounter")
+    @RequestMapping("/controller/encounter")
     public String test() {
 
         return "hello, monsters";
